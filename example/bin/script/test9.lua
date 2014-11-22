a86eec397f770ef1ce722e21e777d18f08097532
ioCx <sanfordlam9@gmail.com>
2014-11-22 02:50:38 -0800

    Updated fireAtTarget example.

--- example/bin/script/test9.lua
+++ example/bin/script/test9.lua
@@ -5,29 +5,48 @@ function bind(f,...)
    end
 end
 
+centerX = 320
+centerY = 240
+
+radius = 200
 theta = 0
 
 function trap(offset)
    -- tx, ty = getTargetPosition()
 
-   x = 320 + 200 * math.sin(theta + math.rad(offset))
-   y = 240 + 200 * math.cos(theta + math.rad(offset))
+   x = centerX + radius * math.sin(theta + math.rad(offset))
+   y = centerY + radius * math.cos(theta + math.rad(offset))
 
    setPosition(x, y)
 
    if (randIntRange(0, 400) == 0) then
-      fireAtTarget(4, nullfunc)
+      fireAtTarget(4, launch)
    end
 
    theta = theta + 0.01
 end
 
+function launch()
+   thisX, thisY = getPosition()
+
+   dx = thisX - centerX
+   dy = thisY - centerY
+
+   -- We should test a tiny bit past the bounds of our circle because on some frames,
+   -- on launch, this bullet will immediately be within this boundary.
+   radiusError = 8
+
+   -- Vanish when bullet leaves trapping circle. Thank you, Pythagoras.
+   if (dx*dx + dy*dy > radius*radius+radiusError) then
+      vanish()
+   end
+end
+
 function main()
    turn = getTurn()
 
-   for i = 0, 39 do
-      print (i)
-      trapi = bind(trap, i * 9)
+   for i = 1, 90 do
+      trapi = bind(trap, i * 4)
       fire(0, 0, trapi)
    end
 
